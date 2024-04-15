729bf0721f66d9ca0d926112f8bc044fe056ced1
AutumnAoi <theone1632@gmail.com>
2024-04-15 10:28:09 +0800

    fix: test case

--- pipeline_test.go
+++ pipeline_test.go
@@ -29,11 +29,15 @@ func TestPipe(t *testing.T) {
 
 	p := NewPipe(ctx)
 	if err := p.Exec(p1); err != nil {
-		t.Error(err)
+		if !errors.Is(err, ErrLastNode) {
+			t.Error(err)
+		}
 	}
 
 	if err := p.Revert(p3); err != nil {
-		t.Error(err)
+		if !errors.Is(err, ErrFirstNode) {
+			t.Error(err)
+		}
 	}
 }
 
@@ -53,18 +57,24 @@ func (p1 *p1Worker) Revert(ctx context.Context) (context.Context, error) {
 	return ctx, nil
 }
 
-type p2Worker struct{}
+type p2Worker struct {
+	Name string
+}
 
 func (p2 *p2Worker) Run(ctx context.Context) (context.Context, error) {
+	fmt.Println("p2")
+
 	p1, ok := ctx.Value(p1Worker{}).(*p1Worker)
 	if !ok {
 		return context.TODO(), errors.New("p1 worker error")
 	}
 
-	fmt.Println("p2")
-	fmt.Println(p1.Name)
+	fmt.Println("worker in ctx", p1.Name)
 
-	return ctx, errors.New("test")
+	p2.Name = "worker 2"
+	ctx = context.WithValue(ctx, p2Worker{}, p2)
+
+	return ctx, nil
 }
 
 func (p2 *p2Worker) Revert(ctx context.Context) (context.Context, error) {
@@ -72,10 +82,20 @@ func (p2 *p2Worker) Revert(ctx context.Context) (context.Context, error) {
 	return ctx, nil
 }
 
-type p3Worker struct{}
+type p3Worker struct {
+	Name string
+}
 
 func (p3 *p3Worker) Run(ctx context.Context) (context.Context, error) {
 	fmt.Println("p3")
+
+	p2, ok := ctx.Value(p2Worker{}).(*p2Worker)
+	if !ok {
+		return context.TODO(), errors.New("p2 worker error")
+	}
+
+	fmt.Println("worker in ctx", p2.Name)
+
 	return ctx, nil
 }
 
